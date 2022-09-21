1e1aaeb060545ee3f95603f007e8269380c158ab
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-09-21 18:18:22 -0300

    feat: create http and controller protocols

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,5 +1,9 @@
 import { HttpRequest, HttpResponse } from "./http";
 
-export interface BaseController<T, U> {
-    route(request: HttpRequest<T>): Promise<HttpResponse<U | null>>;
+// export interface BaseController<T, U> {
+//     route(request: HttpRequest<T>): Promise<HttpResponse<U | null>>;
+// }
+
+export interface Controller {
+  handle(httpRequest: HttpRequest): Promise<HttpResponse>;
 }
--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,12 +1,21 @@
-export interface HttpRequest<T> {
-  body: T;
-  // queryParams: NodeJS.Dict<string | string[]>;
-  pathParams: NodeJS.Dict<string | string[]>;
-  headers: NodeJS.Dict<string | string[]>;
+// export interface HttpRequest<T> {
+//   body: T;
+//   // queryParams: NodeJS.Dict<string | string[]>;
+//   pathParams: NodeJS.Dict<string | string[]>;
+//   headers: NodeJS.Dict<string | string[]>;
+// }
+
+// export interface HttpResponse<T> {
+//   body: T;
+//   status: number;
+//   headers: NodeJS.Dict<string | string[]>;
+// }
+
+export interface HttpRequest {
+  body?: any;
 }
 
-export interface HttpResponse<T> {
-  body: T;
-  status: number;
-  headers: NodeJS.Dict<string | string[]>;
+export interface HttpResponse {
+  statusCode: number;
+  body: any;
 }
