7fa94961147567ab7cc944e3354389d9c948d12b
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-09-21 18:19:08 -0300

    feat: ensure InsertBillController be async

--- src/presentation/controllers/bill/insert-bill.ts
+++ src/presentation/controllers/bill/insert-bill.ts
@@ -1,5 +1,7 @@
-export class InsertBillController {
-  handle(httpRequest: any): any {
+import { Controller } from "../../protocols/controller";
+
+export class InsertBillController implements Controller {
+  async handle(httpRequest: any): Promise<any> {
     return {
       statusCode: 400,
       body: "params name is not provided",
