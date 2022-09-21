15fcc65e2f550697db9c1cea9e84e4c45cb4c7da
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-09-21 18:35:38 -0300

    test: ensure InsertBillController return 400 if no required fields is provided

--- src/presentation/controllers/bill/bill.spec.ts
+++ src/presentation/controllers/bill/bill.spec.ts
@@ -16,4 +16,54 @@ describe("Insert bill controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual("param name is not provided");
   });
+
+  it("Should return 400 if no value is provided", async () => {
+    const controller = new InsertBillController();
+
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        expireDate: "11/01/1999",
+        daysBeforeRemember: 5,
+      },
+    };
+
+    const httpResponse = await controller.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual("param value is not provided");
+  });
+
+  it("Should return 400 if no expireDate is provided", async () => {
+    const controller = new InsertBillController();
+
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        value: "valid_value",
+        daysBeforeRemember: 5,
+      },
+    };
+
+    const httpResponse = await controller.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual("param expireDate is not provided");
+  });
+
+  it("Should return 400 if no daysBeforeRemember is provided", async () => {
+    const controller = new InsertBillController();
+
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        value: "valid_value",
+        expireDate: "11/01/1999",
+      },
+    };
+
+    const httpResponse = await controller.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      "param daysBeforeRemember is not provided"
+    );
+  });
 });
