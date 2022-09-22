4e4400cb46832e39fedbab1a554628d3927e9462
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-09-22 19:17:57 -0300

    feat: ensure InsertBillController return 200 if data is provided

--- src/presentation/controllers/bill/insert-bill.ts
+++ src/presentation/controllers/bill/insert-bill.ts
@@ -1,19 +1,42 @@
+import { InsertBill } from "../../../domain/usecases/insert-bill";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badResquest } from "../../helpers/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest } from "../../protocols/http";
 
 export class InsertBillController implements Controller {
-  async handle(httpRequest: HttpRequest) {
-    return verifyFields(httpRequest);
+  private readonly insertBill: InsertBill;
+
+  constructor(insertBill: InsertBill) {
+    this.insertBill = insertBill;
   }
-}
 
-function verifyFields(httpRequest: HttpRequest) {
-  const requiredFields = ["name", "value", "expireDate", "daysBeforeRemember"];
+  async handle(httpRequest: HttpRequest) {
+    const requiredFields = [
+      "name",
+      "value",
+      "expireDate",
+      "daysBeforeExpireDateToRemember",
+    ];
 
-  for (const field of requiredFields) {
-    if (!httpRequest.body[field])
-      return badResquest(new MissingParamError(field));
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field])
+        return badResquest(new MissingParamError(field));
+    }
+
+    const { name, value, expireDate, daysBeforeExpireDateToRemember } =
+      httpRequest.body;
+
+    const bill = await this.insertBill.insert({
+      name,
+      value,
+      expireDate,
+      daysBeforeExpireDateToRemember,
+    });
+
+    return {
+      statusCode: 200,
+      body: bill,
+    };
   }
 }
