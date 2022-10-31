e12b7968601b4965659c85624c68554ceaf04165
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-10-31 06:15:50 -0300

    feat: ensure sqlite is connected before run integration tests

--- src/main/routes/bill-routes.test.ts
+++ src/main/routes/bill-routes.test.ts
@@ -1,8 +1,15 @@
 import request from "supertest";
+import { SqliteHelper } from "../../infra/db/sqlite/helpers/sqlite-helper";
 
 import app from "../config/app";
 
 describe("Bill Routes", () => {
+  beforeAll(async () => {
+    SqliteHelper.connect();
+    SqliteHelper.generateTableBills();
+    await SqliteHelper.bill.sync();
+  });
+
   it("Should return an bill on success", async () => {
     await request(app)
       .post("/bill")
