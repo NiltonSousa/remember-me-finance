1a6cf1fed8bffa31b4edd286c922f3913bd4c3f0
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-11-29 09:47:09 -0300

    test: ensure InsertNotificationController returns 500 if throws

---
+++ src/presentation/controllers/notification/notification.spec.ts
@@ -0,0 +1,54 @@
+import { NotificationModel } from "../../../domain/models";
+import {
+  InsertNotification,
+  InsertNotificationModel,
+} from "../../../domain/usecases";
+import { serverError } from "../../helpers/http-helper";
+import { InsertNotificationController } from "./insert-notification";
+
+describe("Insert Notification Controller", () => {
+  const makeInsertNotification = () => {
+    class InsertNotificationStub implements InsertNotification {
+      insert(
+        notification: InsertNotificationModel
+      ): Promise<NotificationModel> {
+        const fakeNotification = {
+          id: "valid_id",
+          billId: "valid_bill_id",
+          type: "valid_type",
+          message: "hello_world",
+        };
+        return new Promise((resolve) => resolve(fakeNotification));
+      }
+    }
+    return new InsertNotificationStub();
+  };
+
+  const makeSutInsert = () => {
+    const insertNotificationStub = makeInsertNotification();
+
+    const sut = new InsertNotificationController(insertNotificationStub);
+
+    return {
+      sut,
+      insertNotificationStub,
+    };
+  };
+
+  it("Should return 500 if InsertNotificationController throws", async () => {
+    const { sut } = makeSutInsert();
+
+    const httpRequest = {
+      body: {
+        billId: "valid_bill_id",
+        type: "valid_type",
+        message: "hello_world",
+      },
+    };
+
+    const response = await sut.handle(httpRequest);
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toEqual(serverError());
+  });
+});
