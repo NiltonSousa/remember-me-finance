8958756e432b32fb8e7d36d19dcd94b8c70b2f3c
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-12-08 19:17:15 -0300

    test: ensure InsertNotificationSqliteRepository returns 200 if data is provided

---
+++ src/infra/db/sqlite/notification-repository/notification.spec.ts
@@ -0,0 +1,61 @@
+import ShortUniqueId from "short-unique-id";
+import { SqliteHelper } from "../helpers/sqlite-helper";
+import { InsertNotificationSqliteRepository } from "./insert-notification";
+
+describe("Notification sqlite repository", () => {
+  const generateId = new ShortUniqueId({ length: 7 });
+  const billId = String(generateId()).toUpperCase();
+  const clientId = String(generateId()).toUpperCase();
+
+  beforeAll(async () => {
+    SqliteHelper.connect("test");
+
+    await SqliteHelper.createClient(
+      clientId,
+      "valid_name",
+      "12345678",
+      "19/01/1999",
+      "user@mail.com",
+      "123",
+      "4"
+    );
+
+    await SqliteHelper.createBill({
+      id: billId,
+      clientId,
+      name: "valid_name",
+      value: "valid_value",
+      expireDate: "19/01/1999",
+      daysBeforeExpireDateToRemember: "5",
+    });
+  });
+
+  afterAll(async () => {
+    const deleteOlderClients = SqliteHelper.client.client.deleteMany();
+    const deleteOlderBills = SqliteHelper.client.bill.deleteMany();
+
+    await SqliteHelper.client.$transaction([
+      deleteOlderClients,
+      deleteOlderBills,
+    ]);
+
+    await SqliteHelper.disconnect();
+  });
+
+  it("Should return a notification when insert is success", async () => {
+    const sut = new InsertNotificationSqliteRepository();
+
+    return await sut
+      .insert({
+        id: "valid_id",
+        billId,
+        type: "valid_type",
+        message: "hello_world",
+      })
+      .then((data) => {
+        expect(data.billId).toEqual(billId);
+        expect(data.type).toEqual("valid_type");
+        expect(data.message).toEqual("hello_world");
+      });
+  });
+});
