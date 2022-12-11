c9008dd5540bce7cb2381cf5b74ded5c0a1d5350
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-12-11 16:24:58 -0300

    test: create notification route

---
+++ src/main/routes/notification-routes.test.ts
@@ -0,0 +1,54 @@
+import ShortUniqueId from "short-unique-id";
+import request from "supertest";
+import { SqliteHelper } from "../../infra/db/sqlite/helpers/sqlite-helper";
+
+import app from "../config/app";
+
+const generateId = new ShortUniqueId({ length: 6 });
+const clientId = String(generateId()).toUpperCase();
+const billId = String(generateId()).toUpperCase();
+
+describe("Notification Routes", () => {
+    beforeAll(async () => {
+      SqliteHelper.connect("test");
+      await SqliteHelper.createClient(
+        clientId,
+        "valid_name",
+        "12345678",
+        "19/01/1999",
+        "user@mail.com",
+        "123",
+        "4"
+      );
+
+      await SqliteHelper.createBill(
+        {
+            id: billId,
+            clientId,
+            value: "valid_value",
+            name: "valid_name",
+            expireDate: "19/01/1999",
+            daysBeforeExpireDateToRemember:"5"
+        }
+      );
+    });
+  
+    afterAll(async () => {
+      const deleteOlderClients = SqliteHelper.client.client.deleteMany();
+  
+      await SqliteHelper.client.$transaction([deleteOlderClients]);
+      await SqliteHelper.disconnect();
+    });
+  
+    it("Should return an notification on success", async () => {
+      await request(app)
+        .post("/notification")
+        .send({
+          billId,
+          type: "valid_type",
+          message: "hello_world",
+        })
+        .expect(200);
+    });
+  });
+  
