0e44265227b8dedfddf34f3236c6f99726b54885
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-12-14 07:09:52 -0300

    test: ensure InsertClientController throws when throw

---
+++ src/presentation/controllers/client/client.spec.ts
@@ -0,0 +1,30 @@
+import { serverError } from "../../helpers";
+import { HttpRequest } from "../../protocols";
+import { InsertClientController } from "./insert-client";
+
+const makeInsertClient = () => {
+  return new InsertClientController();
+};
+
+describe("Insert Client Controller", () => {
+  it("Should return 500 if InsertClientController throws", async () => {
+    const sut = makeInsertClient();
+
+    const httpRequest: HttpRequest = {
+      body: {
+        name: "valid_name",
+        cpf: "valid_cpf",
+        message: "valid_message",
+        birthdate: "valid_birthdate",
+        email: "valid_email",
+        phoneNumber: "valid_number",
+        billsCount: "valid_bills_count_id",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse).toEqual(serverError());
+  });
+});
