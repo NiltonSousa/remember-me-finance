52333ade3267bc695d5fd22f2d33e0e036774968
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-12-26 11:04:08 -0300

    test: should call insertClientRepository with correct values

--- src/data/insert-client/db-insert-client.spec.ts
+++ src/data/insert-client/db-insert-client.spec.ts
@@ -1,13 +1,42 @@
+import { ClientModel } from "../../domain/models";
+import { DbInsertClientModel, InsertClientRepository } from "../protocols";
 import { DbInsertClient } from "./db-insert-client";
 
-const makeSut = () => {
-  const sut = new DbInsertClient();
+const makeInsertClientRepositoryStub = (): InsertClientRepository => {
+  class InsertClientRepositoryStub implements InsertClientRepository {
+    async insert(client: DbInsertClientModel): Promise<ClientModel> {
+      const fakeClient = {
+        id: "valid_id",
+        name: "valid_name",
+        cpf: "valid_cpf",
+        birthdate: "19/01/1999",
+        email: "valid@mail.com",
+        phoneNumber: "valid_number",
+        billsCount: "0",
+      };
 
-  return { sut };
+      return new Promise((resolve) => resolve(fakeClient));
+    }
+  }
+
+  return new InsertClientRepositoryStub();
+};
+
+const makeSut = () => {
+  const insertClientRepositoryStub = makeInsertClientRepositoryStub();
+  const sut = new DbInsertClient(insertClientRepositoryStub);
+
+  return { sut, insertClientRepositoryStub };
 };
 describe("DbInsertClient use case", () => {
   it("Should throw if InsertClientRepository throws", async () => {
-    const { sut } = makeSut();
+    const { sut, insertClientRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(insertClientRepositoryStub, "insert")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
 
     const clientData = {
       name: "valid_name",
@@ -21,5 +50,31 @@ describe("DbInsertClient use case", () => {
     const promise = sut.insert(clientData);
 
     await expect(promise).rejects.toThrow();
+  });
+
+  it("Should call insertClientRepository with correct values", async () => {
+    const { sut, insertClientRepositoryStub } = makeSut();
+
+    const clientData = {
+      name: "valid_name",
+      cpf: "valid_cpf",
+      birthdate: "19/01/1999",
+      email: "valid@mail.com",
+      phoneNumber: "valid_number",
+      billsCount: "0",
+    };
+
+    const insertSpy = jest.spyOn(insertClientRepositoryStub, "insert");
+
+    await sut.insert(clientData);
+
+    expect(insertSpy).toHaveBeenCalledWith({
+      name: "valid_name",
+      cpf: "valid_cpf",
+      birthdate: "19/01/1999",
+      email: "valid@mail.com",
+      phoneNumber: "valid_number",
+      billsCount: "0",
+    });
   });
 });
