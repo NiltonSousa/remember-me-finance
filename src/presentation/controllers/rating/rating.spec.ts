feaae733bf55543919686e5ceca1f73526d78b48
Nilton Sousa <nilton.jsousa@hotmail.com>
2023-01-09 08:00:01 -0300

    test: should return 400 if no clientId and grande is provided

--- src/presentation/controllers/rating/rating.spec.ts
+++ src/presentation/controllers/rating/rating.spec.ts
@@ -1,7 +1,8 @@
 import { RatingModel } from "../../../domain/models";
 import { InsertRating, InsertRatingModel } from "../../../domain/usecases";
+import { MissingParamError } from "../../errors";
 import { serverError } from "../../helpers/http-helper";
-import { InsertRatingController } from "./rating";
+import { InsertRatingController } from "./insert-rating";
 
 describe("Insert Rating Controller", () => {
   const makeInsertRating = () => {
@@ -22,7 +23,7 @@ describe("Insert Rating Controller", () => {
   const makeSutInsert = () => {
     const insertRatingStub = makeInsertRating();
 
-    const sut = new InsertRatingController();
+    const sut = new InsertRatingController(insertRatingStub);
 
     return {
       sut,
@@ -32,6 +33,10 @@ describe("Insert Rating Controller", () => {
 
   it("Should return 500 if InsertRatingController throws", async () => {
     const { sut, insertRatingStub } = makeSutInsert();
+
+    jest.spyOn(insertRatingStub, "insert").mockImplementationOnce(() => {
+      return new Promise((resolve, reject) => reject(new Error()));
+    });
 
     const httpRequest = {
       body: {
@@ -44,5 +49,33 @@ describe("Insert Rating Controller", () => {
 
     expect(response.statusCode).toBe(500);
     expect(response).toEqual(serverError());
+  });
+
+  it("Should return 400 when no clientId is provided", async () => {
+    const { sut } = makeSutInsert();
+
+    const httpRequest = {
+      body: {
+        grade: "5",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("clientId"));
+  });
+
+  it("Should return 400 when no grade is provided", async () => {
+    const { sut } = makeSutInsert();
+
+    const httpRequest = {
+      body: {
+        clientId: "valid_client_id",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("grade"));
   });
 });
