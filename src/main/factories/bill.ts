d2b034e9c804ba2cde5d0c4d2be1606739a6a38b
Nilton Sousa <nsousa@zak.app>
2023-03-11 14:04:24 -0300

    feat: create list bill route

--- src/main/factories/bill.ts
+++ src/main/factories/bill.ts
@@ -1,6 +1,9 @@
 import { InsertBillController } from "../../presentation/controllers/bill/insert-bill";
 import { DbInsertBill } from "../../data/insert-bill/db-insert-bill";
 import { InsertBillSqliteRepository } from "../../infra/db/postgresql/bill-repository/insert-bill";
+import { ListBillController } from "../../presentation/controllers/bill";
+import { ListBillSqliteRepository } from "../../infra/db/postgresql/bill-repository/list-bill";
+import { DbListBill } from "../../data/list-bill/db-list-bill";
 
 export const makeInsertBillController = (): InsertBillController => {
   const billSqliteRepository = new InsertBillSqliteRepository();
@@ -8,4 +11,12 @@ export const makeInsertBillController = (): InsertBillController => {
   const dbInsertBill = new DbInsertBill(billSqliteRepository);
 
   return new InsertBillController(dbInsertBill);
+};
+
+export const makeListBillController = (): ListBillController => {
+  const billSqliteRepository = new ListBillSqliteRepository();
+
+  const dbListBill = new DbListBill(billSqliteRepository);
+
+  return new ListBillController(dbListBill);
 };
--- src/main/routes/bill-routes.ts
+++ src/main/routes/bill-routes.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express-route-adapter";
-import { makeInsertBillController } from "../factories/bill";
+import {
+  makeInsertBillController,
+  makeListBillController,
+} from "../factories/bill";
 
 export const billRouter = Router();
+
 billRouter.post("/bill", adaptRoute(makeInsertBillController()));
+billRouter.get("/bill", adaptRoute(makeListBillController()));
