7fd8c3a8e2851970dc561f5b625125bd2e2501ce
Nilton Sousa <nsousa@zak.app>
2023-03-12 14:59:21 -0300

    feat: create update bill router

--- src/main/factories/bill.ts
+++ src/main/factories/bill.ts
@@ -7,6 +7,9 @@ import { DbListBill } from "../../data/list-bill/db-list-bill";
 import { DeleteBillController } from "../../presentation/controllers/bill/delete-bill";
 import { DeleteBillSqliteRepository } from "../../infra/db/postgresql/bill-repository/delete-bill";
 import { DbDeleteBill } from "../../data/delete-bill/db-delete-bill";
+import { UpdateBillSqliteRepository } from "../../infra/db/postgresql/bill-repository/update-bill";
+import { UpdateBillController } from "../../presentation/controllers/bill/update-bill";
+import { DbUpdateBill } from "../../data/update-bill/db-update-bill";
 
 export const makeInsertBillController = (): InsertBillController => {
   const billSqliteRepository = new InsertBillSqliteRepository();
@@ -14,6 +17,14 @@ export const makeInsertBillController = (): InsertBillController => {
   const dbInsertBill = new DbInsertBill(billSqliteRepository);
 
   return new InsertBillController(dbInsertBill);
+};
+
+export const makeUpdateBillController = (): UpdateBillController => {
+  const billSqliteRepository = new UpdateBillSqliteRepository();
+
+  const dbUpdateBill = new DbUpdateBill(billSqliteRepository);
+
+  return new UpdateBillController(dbUpdateBill);
 };
 
 export const makeListBillController = (): ListBillController => {
--- src/main/routes/bill-routes.ts
+++ src/main/routes/bill-routes.ts
@@ -4,10 +4,12 @@ import {
   makeDeleteBillController,
   makeInsertBillController,
   makeListBillController,
+  makeUpdateBillController,
 } from "../factories/bill";
 
 export const billRouter = Router();
 
 billRouter.post("/bill", adaptRoute(makeInsertBillController()));
+billRouter.put("/bill", adaptRoute(makeUpdateBillController()));
 billRouter.get("/bill", adaptRoute(makeListBillController()));
 billRouter.delete("/bill", adaptRoute(makeDeleteBillController()));
