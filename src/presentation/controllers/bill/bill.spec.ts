fb80518f2a83d5e65096812992cf9ee2c84a1eb2
Nilton Sousa <nsousa@zak.app>
2023-03-12 14:56:48 -0300

    test: should return a bill when UpdateBillController success

--- src/presentation/controllers/bill/bill.spec.ts
+++ src/presentation/controllers/bill/bill.spec.ts
@@ -6,9 +6,11 @@ import {
   InsertBillModel,
   ListBill,
 } from "../../../domain/usecases/index";
+import { UpdateBill } from "../../../domain/usecases/update-bill";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { serverError } from "../../helpers/http-helper";
 import { DeleteBillController } from "./delete-bill";
+import { UpdateBillController } from "./update-bill";
 
 export interface SutTypesInsert {
   sut: InsertBillController;
@@ -30,6 +32,23 @@ const makeInsertBill = () => {
     }
   }
   return new InsertBillStub();
+};
+
+const makeUpdateBill = () => {
+  class UpdateBillStub implements UpdateBill {
+    update(bill: BillModel): Promise<BillModel> {
+      const fakeBill = {
+        id: "valid_bill_id",
+        clientId: "valid_id",
+        name: "valid_name",
+        value: "valid_value",
+        expireDate: "01/01/1999",
+        daysBeforeExpireDateToRemember: "5",
+      };
+      return new Promise((resolve) => resolve(fakeBill));
+    }
+  }
+  return new UpdateBillStub();
 };
 
 const makeListBill = () => {
@@ -73,6 +92,17 @@ const makeSutInsert = (): SutTypesInsert => {
   return {
     sut,
     insertBillStub,
+  };
+};
+
+const makeSutUpdate = () => {
+  const updateBillStub = makeUpdateBill();
+
+  const sut = new UpdateBillController(updateBillStub);
+
+  return {
+    sut,
+    updateBillStub,
   };
 };
 
@@ -203,6 +233,111 @@ describe("Insert bill controller", () => {
 
     const httpRequest = {
       body: {
+        clientId: "valid_id",
+        name: "valid_name",
+        value: "valid_value",
+        expireDate: "01/01/1999",
+        daysBeforeExpireDateToRemember: "5",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse).toEqual(serverError());
+  });
+});
+
+describe("Update bill controller", () => {
+  it("Should return 400 if no name is provided", async () => {
+    const { sut } = makeSutUpdate();
+
+    const httpRequest = {
+      body: {
+        id: "valid_bill_id",
+        clientId: "valid_id",
+        value: "valid_value",
+        expireDate: "11/01/1999",
+        daysBeforeExpireDateToRemember: "5",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
+  });
+
+  it("Should return 400 if no value is provided", async () => {
+    const { sut } = makeSutUpdate();
+
+    const httpRequest = {
+      body: {
+        id: "valid_bill_id",
+        clientId: "valid_id",
+        name: "valid_name",
+        expireDate: "11/01/1999",
+        daysBeforeExpireDateToRemember: "5",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("value"));
+  });
+
+  it("Should return 400 if no expireDate is provided", async () => {
+    const { sut } = makeSutUpdate();
+
+    const httpRequest = {
+      body: {
+        id: "valid_bill_id",
+        clientId: "valid_id",
+        name: "valid_name",
+        value: "valid_value",
+        daysBeforeExpireDateToRemember: "5",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("expireDate"));
+  });
+
+  it("Should return 200 if data is provided", async () => {
+    const { sut } = makeSutUpdate();
+
+    const httpRequest = {
+      body: {
+        id: "valid_bill_id",
+        clientId: "valid_id",
+        name: "valid_name",
+        value: "valid_value",
+        expireDate: "01/01/1999",
+        daysBeforeExpireDateToRemember: "5",
+      },
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: "valid_bill_id",
+      clientId: "valid_id",
+      name: "valid_name",
+      value: "valid_value",
+      expireDate: "01/01/1999",
+      daysBeforeExpireDateToRemember: "5",
+    });
+  });
+
+  it("Should return 500 if UpdateBillController throws", async () => {
+    const { sut, updateBillStub } = makeSutUpdate();
+
+    jest.spyOn(updateBillStub, "update").mockImplementationOnce(() => {
+      return new Promise((resolver, reject) => reject(new Error()));
+    });
+
+    const httpRequest = {
+      body: {
+        id: "valid_bill_id",
         clientId: "valid_id",
         name: "valid_name",
         value: "valid_value",
